f3287022e07ecf34aa4f32c730d37ecb26bb285e
Evgeny <admin@Evgenys-MacBook-Pro.local>
2023-12-16 18:46:54 +0300

    testing create account

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -1,5 +1,8 @@
 import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { TonConnectUI } from "@tonconnect/ui-react";
 import { TonClient } from "ton";
+import { Address, SenderArguments, } from "ton-core"
+
 
 // TODO
 // use as provideer
@@ -7,5 +10,30 @@ import { TonClient } from "ton";
 const clientPromise = getHttpEndpoint({
     network: "testnet"
 }).then(endpoint => new TonClient({ endpoint }));
+
+// Overload Sender
+export interface Sender {
+    readonly address?: Address;
+    send(args: SenderArguments): Promise<void>;
+}
+
+export const getSender = (tonConnectUI: TonConnectUI): Sender | null =>  {
+    if (!tonConnectUI.connected) return null;
+    return {
+        address: Address.parse(tonConnectUI.account?.address!),
+        send: async (args: SenderArguments) => {
+            await tonConnectUI.sendTransaction({
+                messages: [
+                {
+                    address: args.to.toString(),
+                    amount: args.value.toString(),
+                    payload: args.body?.toBoc().toString("base64"),
+                },
+                ],
+                validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
+            });
+        },
+    };
+}
 
 export default clientPromise;
--- src/routes/Solo/Boarding/Check/index.tsx
+++ src/routes/Solo/Boarding/Check/index.tsx
@@ -7,6 +7,10 @@ import { Colors } from "../../../../helpers/colors";
 import * as model from '../../../../model';
 
 import "./style.css"
+import clientPromise, { getSender } from "../../../../controllers/client";
+import { SoloMaster } from "neobase/wrappers/SoloMaster";
+import { Address, toNano } from "ton-core";
+import { useTonConnectUI } from "@tonconnect/ui-react";
 
 const risk2comission = (risk: number) => (100 / (1 << risk)).toFixed(2) + '%';
 
@@ -14,16 +18,32 @@ export const Check = () => {
 
     const navigate = useNavigate();
 
+    const [tonConnectUi] = useTonConnectUI();
+
     useEffect(() => {
 
         const back = () => navigate(-1);
         WebApp.BackButton.show();
         WebApp.BackButton.onClick(back);
+        
+        const contract = SoloMaster.createFromAddress(Address.parse(import.meta.env.VITE_SOLO_MASTER_TEST_ADDRESS));
 
-        const next = () => {};
+        const next = () => {
+            
+            clientPromise.then(client => {
+                const api = client.open(contract);
+                const sender = getSender(tonConnectUi)!;
+                api.sendCreateAccount(sender, toNano(0.05), {
+                    goalAmount: toNano(model.solo.state.goalAmount),
+                    risk: model.solo.state.risk,
+                    heroId: 2
+                });
+                console.log('Create Account Sent');
+            })
+        };
         WebApp.MainButton.enable();
         WebApp.setHeaderColor(Colors.BLUE);
-        WebApp.MainButton.setText(`do ${Kaomoji.REFLECTED.MAGIC} magic`);
+        WebApp.MainButton.setText(`${Kaomoji.REFLECTED.MAGIC} do magic`);
         WebApp.MainButton.onClick(next);
         WebApp.MainButton.show();
 
