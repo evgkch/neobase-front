0f80506a2a502a6f5459f41e6fa72ae7b393c3aa
Evgeny <admin@Evgenys-MacBook-Pro.local>
2023-12-16 19:08:36 +0300

    test

--- src/routes/Solo/Boarding/Check/index.tsx
+++ src/routes/Solo/Boarding/Check/index.tsx
@@ -7,10 +7,10 @@ import { Colors } from "../../../../helpers/colors";
 import * as model from '../../../../model';
 
 import "./style.css"
-import clientPromise, { getSender } from "../../../../controllers/client";
-import { SoloMaster } from "neobase/wrappers/SoloMaster";
-import { Address, toNano } from "ton-core";
-import { useTonConnectUI } from "@tonconnect/ui-react";
+
+import { Op } from "neobase/wrappers/constants";
+import { Address, beginCell, toNano } from "ton-core";
+import { SendTransactionRequest, useTonConnectUI } from "@tonconnect/ui-react";
 
 const risk2comission = (risk: number) => (100 / (1 << risk)).toFixed(2) + '%';
 
@@ -26,20 +26,48 @@ export const Check = () => {
         WebApp.BackButton.show();
         WebApp.BackButton.onClick(back);
         
-        const contract = SoloMaster.createFromAddress(Address.parse(import.meta.env.VITE_SOLO_MASTER_TEST_ADDRESS));
+        //const contract = SoloMaster.createFromAddress(Address.parse(import.meta.env.VITE_SOLO_MASTER_TEST_ADDRESS));
 
         const next = () => {
+
+            const body = beginCell()
+                .storeUint(Op.create_account, 32)
+                .storeRef(
+                    beginCell()
+                        .storeUint(1, 32)
+                        .storeCoins(toNano(model.solo.state.goalAmount))
+                        .storeUint(model.solo.state.risk, 7)
+                        .endCell()
+                )
+                .endCell();
+
+            const transaction = {
+                validUntil: Math.floor(Date.now() / 1000) + 360,
+                messages: [
+                    {
+                        address: Address.parse(import.meta.env.VITE_SOLO_MASTER_TEST_ADDRESS).toRawString(),
+                        amount: toNano(0.1).toString(),
+                        payload: body.toBoc().toString("base64") // payload with comment in body
+                    }
+                ]
+            } as SendTransactionRequest;
+
+            tonConnectUi.sendTransaction(transaction).
+                then(res => {console.log(res)})
+                .catch(err => {console.log(err)})
             
-            clientPromise.then(client => {
-                const api = client.open(contract);
-                const sender = getSender(tonConnectUi)!;
-                api.sendCreateAccount(sender, toNano(0.05), {
-                    goalAmount: toNano(model.solo.state.goalAmount),
-                    risk: model.solo.state.risk,
-                    heroId: 2
-                });
-                console.log('Create Account Sent');
-            })
+            // clientPromise.then(client => {
+            //     const api = client.open(contract);
+            //     const sender = getSender(tonConnectUi)!;
+            //     api.sendCreateAccount(sender, toNano(0.1), {
+            //         goalAmount: toNano(model.solo.state.goalAmount),
+            //         risk: model.solo.state.risk,
+            //         heroId: 1
+            //     });
+
+                
+            //     console.log('Create Account Sent');
+            // })
         };
         WebApp.MainButton.enable();
         WebApp.setHeaderColor(Colors.BLUE);
